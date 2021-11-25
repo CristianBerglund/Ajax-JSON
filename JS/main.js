11769d9b2b1b1e143e8151a2a8a82889fae41dc8
Cristian Berglund <Cristian.berglund@hotmail.se>
2021-11-25 15:57:46 +0100

    first-fetch-done

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,33 @@
 let inputfield = document.getElementById("Inputfield")
 let content = document.getElementById("content")
-let searchBtn = document.getElementById("search-data")
+let searchBtn = document.getElementById("search-serie")
 
 searchBtn.addEventListener("click", fetchData);
 
 async function fetchData() {
     try {
-        let response = await fetch("https://api.tvmaze.com/search/shows?q=superman")
+        let response = await fetch("https://api.tvmaze.com/shows/" + inputfield.value)
         if (!response.ok) {
             throw new Error('Some network problems')
         }
         let data = await response.json()
-        console.log(data)
+        // console.log(data.genres)
+        // console.log(data)
+        console.log(data.rating.average)
+        let headline = data.name
+        let genres   = data.genres
+        let image    = data.image.medium
+        let summary  = data.summary
+        let rating   = data.rating.average
+
+        content.innerHTML = `
+        <h1>${headline}</h1>  
+        <p class="genres">Genres: ${genres.join(" ")}</p> 
+        <p>Rating: ${rating} </p>
+        <img class="img" src="${image}">
+        ${summary}
+        `
+
     } catch (error) {
         content.innerHTML = "Something went wrong, please try again later"
    }
